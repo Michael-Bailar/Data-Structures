576e132fc84ffc333e446e16d2763b738dd02cd5
Michael-B1764 <mikebailar@gmail.com>
2020-07-13 13:52:54 -0700

    done with stack

--- stack/stack.py
+++ stack/stack.py
@@ -9,24 +9,67 @@ return elements in Last In First Out order.
    Make sure the Stack tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
+        ANSWER: Linked list is more efficient. with an array, you need to keep
+            track of the index of the last item and  use that to edit it. With
+            a linked list, you simply pull the last piece of the end.
 """
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
 
+    def get_value(self):
+        return self.value
+    
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
 
 class Stack:
     def __init__(self):
-        self.size = 0
-        self.storage = []
+        self.head = None
+        self.tail = None
 
     def __len__(self):
-        return len(self.storage)
+        if self.head == None:
+            return 0
+        else:
+            value = self.head
+            count = 0
+            while(value):
+                count += 1
+                value = value.get_next()
+            return count
+        
 
     def push(self, value):
-        self.size += 1
-        return self.storage.append(value)
-       
+        new_node = Node(value, None)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
 
     def pop(self):
-        if len(self.storage) == 0:
-            pass
-        else:
-            return self.storage.pop()
+        if not self.head:
+            return None
+
+        if self.head is self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return value
+
+        current = self.head
+
+        while current.get_next() is not self.tail:
+            current = current.get_next()
+
+        value = self.tail.get_value()
+        self.tail = current
+        self.tail.next_node = None
+        return value
+    
